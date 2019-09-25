325a9647bd922deebba14733f5a42f4b580aa6d1
tclaude94 <23203061+tclaude94@users.noreply.github.com>
2019-09-25 20:41:46 +0200

    Add toastr messsage

--- routes/help.js
+++ routes/help.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.render('help');
+    res.render('help', { toastrNotif: false});
 });
 
 module.exports = router;
