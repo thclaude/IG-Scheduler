5251ec7a062a8e9b798f0dde8ddc7e0e2de3c6c4
tclaude94 <23203061+tclaude94@users.noreply.github.com>
2020-09-06 20:22:56 +0200

    Add henallux maintenance alert every last friday of month

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,6 +2,16 @@
   <v-app>
     <Navbar />
     <v-main>
+      <v-alert
+          prominent
+          text icon="mdi-alert"
+          color="warning"
+          class="mb-5 text-center"
+          v-if="isLastFridayOfMonth">
+        <strong>Avertissement - Maintenance Henallux</strong>
+        <br/>
+        Aujourd'hui, comme tous les derniers vendredis du mois, a lieu une maintenance du système informatique Henallux.<br/>La génération de calendrier sera perturbée le temps de cette maintenance.
+      </v-alert>
       <v-container fluid class="main-container">
         <router-view/>
       </v-container>
@@ -18,6 +28,26 @@ export default {
   components: {
     Navbar,
     Footer
+  },
+  computed: {
+    getLastDayOfMonth() {
+      const today = new Date();
+      return new Date(today.getFullYear(), today.getMonth()+1, 0);
+    },
+
+    getLastFridayOfMonth(){
+      let lastDayOfMonth = this.getLastDayOfMonth;
+      while(lastDayOfMonth.getDay() !== 5){
+        lastDayOfMonth.setDate(lastDayOfMonth.getDate() - 1);
+      }
+      return lastDayOfMonth;
+    },
+
+    isLastFridayOfMonth(){
+      const today = new Date();
+      const lastFriday = this.getLastFridayOfMonth;
+      return today.getDate() === lastFriday.getDate();
+    }
   }
 };
 </script>
